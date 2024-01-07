6b37b9b5c02540026aceea0a9021cda8bded3a8e
Luiz Kohl <bykohl@gmail.com>
2024-01-07 05:35:17 -0300

    Department entity class

--- src/db/DB.java
+++ src/db/DB.java
@@ -8,6 +8,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
 public class DB {
 

970131231ba551ff502f97f55e18102d3d56c333
Luiz Kohl <bykohl@gmail.com>
2024-01-08 06:47:43 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,8 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST 1: Seller findById ===");
@@ -44,6 +47,13 @@ public class Program {
 		sellerDao.update(seller);
 		System.out.println("Update complete");
 
+		System.out.println("\n=== TEST 6: Seller delete ===");
+		System.out.print("Enter Id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete complete");
+
+		sc.close();
 	}
 
 }
